68cd7c44e60cb406885916cb8fff1662b40a882e
teller2016 <teller2016@ajou.ac.kr>
2021-12-02 22:35:30 +0900

    chapter 4 done

---
+++ Basic/print.py
@@ -0,0 +1,20 @@
+# sep 파라미터
+print('A', 'B', sep='+')
+
+# end 파라미터
+## 줄바꿈을 없앨 수 있다
+print('aa', end='')
+print('bb')
+
+# join()
+a = ['a','b','c']
+print(''.join(a))
+
+# .format()
+print('{} and {}'.format(1,2))
+
+# f''
+f1 = 4
+f2 = 'apple'
+print(f'{f1} and {f2}')
+
